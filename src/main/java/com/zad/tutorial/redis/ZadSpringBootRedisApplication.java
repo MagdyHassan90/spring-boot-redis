603122efd5223b97a2971dbee170e99126ff7538
Magdy Hassan <MagdyHassan.work@gmail.com>
2023-04-27 03:14:55 +0200

    987

---
+++ src/main/java/com/zad/tutorial/redis/ZadSpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zad.tutorial.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadSpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadSpringBootRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zad/tutorial/redis/ZadSpringBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zad.tutorial.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadSpringBootRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
